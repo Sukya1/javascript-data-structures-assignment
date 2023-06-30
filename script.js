c64162b2103652ff35aff41b3014cd0fa92645d0
Sukya1 <williamssukya@yahoo.com>
2023-06-30 10:04:22 -0400

    completed assignement

--- script.js
+++ script.js
@@ -32,7 +32,28 @@ let mySampleObject = {
 //Hint: go to https://unsplash.com for some great, high-res pics and search up whatever animal you want!
 
 //Make sure to make at least 3 animal objects with these keys inside of the myZooAnimals array!
+let panda = {
+  animalType: "Panda",
+  name: "Poo the Panda",
+  age: 2,
+  pictureURL: "https://images.chinahighlights.com/allpicture/2020/08/826a518ea9fc46d4b5adcc38_cut_800x500_61.jpg",
+};
+let poodle = {
+  animalType: "poodle",
+  name: "Lucky the Poodle",
+  age: 5,
+  pictureURL: "https://cf.ltkcdn.net/dogs/dog-breeds/images/orig/322772-1600x1066-miniature-poodle-guide.jpg",
+};
+let zebra = {
+  animalType: "Zebra",
+  name: "Zoey the Zebra",
+  age: 4,
+  pictureURL: "https://cdn.mos.cms.futurecdn.net/HjFE8NKWuCmgfHCcndJ3rK.jpg",
+};
 
-let myZooAnimals;
-
+let myZooAnimals = 
+    [panda, poodle, zebra]
+;
+//Test 
+console.log(myZooAnimals);
 //END OF ASSIGNMENT
